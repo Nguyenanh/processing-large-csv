a20a934a38a4c72c95c9f6202dfd6f9f29f2ea18
Nguyen Anh <cauut2117610@gmail.com>
2017-10-22 15:34:49 +0700

    first commit

---
+++ generate_csv.rb
@@ -0,0 +1,19 @@
+require 'csv'
+require_relative './helpers'
+headers = ['id', 'name', 'email', 'city', 'street', 'country']
+
+name    = "Pink Panther"
+email   = "pink.panther@example.com"
+city    = "Pink City"
+street  = "Pink Road"
+country = "Pink Country"
+
+print_memory_usage do
+  print_time_spent do
+    CSV.open('data.csv', 'w', write_headers: true, headers: headers, quote_char: "|", col_sep: ',') do |csv|
+      1_000_000.times do |i|
+        csv << [i, name, email, city, street, country]
+      end
+    end
+  end
+end
